07899c4d61350fc01e5caa1f63c49987aca7d9e8
Josh Nussbaum <joshnuss@gmail.com>
2019-12-21 01:06:33 -0500

    Fix tab index

--- src/components/Grid.svelte
+++ src/components/Grid.svelte
@@ -18,7 +18,7 @@
         <th>{i+1}</th>
 
         {#each row as cell, j}
-          <td class="input" tabindex={j}><Cell bind:cell={cell}/></td>
+          <td class="input" tabindex={i}><Cell bind:cell={cell}/></td>
         {/each}
       </tr>
     {/each}
