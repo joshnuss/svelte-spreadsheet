07a0946aaf0dbca0c76bcd906ffef9604df78642
Josh Nussbaum <joshnuss@gmail.com>
2019-12-21 02:03:38 -0500

    Values are strings by default

--- src/store.js
+++ src/store.js
@@ -5,8 +5,8 @@ export const position = writable({row: 0, column: 0})
 export const data = writable({
   columns: ['A', 'B', 'C'],
   rows: [
-    [{value: 1},{value: 2}, {value: 3}],
-    [{value: 4},{value: 5}, {value: 6}],
+    [{value: '1'},{value: '2'}, {value: '3'}],
+    [{value: '4'},{value: '5'}, {value: '6'}],
   ]
 })
 
