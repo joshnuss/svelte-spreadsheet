7209ecdd7fa2695f30a16798d5dbaf383d47a015
Josh Nussbaum <joshnuss@gmail.com>
2019-12-21 00:49:21 -0500

    Add Grid and Cell components

---
+++ src/components/Cell.svelte
@@ -0,0 +1,22 @@
+<span>11</span>
+
+<style>
+  input {
+    border: none;
+    margin: 0;
+    display: block;
+    width: 100%;
+  }
+
+  span, input {
+    padding: 0.4em;
+  }
+
+  input:focus {
+    outline: none;
+  }
+
+  input.number {
+    text-align: right;
+  }
+</style>
---
+++ src/components/Grid.svelte
@@ -0,0 +1,76 @@
+<script>
+  import Cell from './Cell.svelte'
+</script>
+
+<table>
+  <thead>
+    <tr>
+      <th width="60"/>
+      <th>A</th>
+      <th>B</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th>1</th>
+      <td class="input" tabindex="1"><Cell/></td>
+      <td class="input" tabindex="1"><Cell/></td>
+    </tr>
+    <tr>
+      <th>2</th>
+      <td class="input" tabindex="2"><Cell/></td>
+      <td class="input" tabindex="2"><Cell/></td>
+    </tr>
+  </tbody>
+</table>
+
+<style>
+  :global(body) {
+    padding: 0;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0px;
+    width: 100%;
+  }
+
+  table td, table th {
+    border: solid 1px #ddd
+  }
+
+  table th {
+    background: #ccc;
+    padding: 0.2em;
+  }
+
+  table tr.header {
+    background: #666;
+    color: #eee;
+    font-size: 0.8rem;
+  }
+  table tr.header td {
+    text-align: left;
+    padding: 0.4rem
+  }
+  table td.label {
+    text-align: left;
+    background: #ccc
+  }
+  table td {
+    padding: 0.3em;
+    text-align: right
+  }
+  table td:focus {
+    outline: 1px solid torquoise;
+  }
+  table td.input {
+    padding: 0;
+  }
+
+  table select {
+    background: transparent;
+    border: none;
+    margin: 0;
+    display: block;
+  }
+</style>
